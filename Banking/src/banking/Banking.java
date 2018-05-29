2bbbf9cc0f04b51b0b9c23306130b8b34875eb46
admin <admin@kalpana>
2018-05-29 15:48:53 +0530

    First commit

---
+++ Banking/src/banking/Banking.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banking;
+
+/**
+ *
+ * @author admin
+ */
+public class Banking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        do{
+        System.out.println("Banking Application");
+        System.out.println("Menu");
+        System.out.println("1.Create a account");
+        System.out.println("Menu");
+        System.out.println("Menu");
+        System.out.println("Menu");
+    }
+    
+}
