fe3e8027e50098082e2b6c2e3ac8564b78f16aba
admin <admin@kalpana>
2018-06-02 20:55:11 +0530

    Fourth commit

--- Banking/src/banking/Banking.java
+++ Banking/src/banking/Banking.java
@@ -1,206 +1,83 @@
-
 package banking;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-import java.util.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+/**
+ *
+ * @author User
+ */
+public class Banking {
+ 
+ create c;
+ 
+ 
+ public Banking() {
+ }
+ 
+ @BeforeClass
+ public static void setUpClass() {
+ }
+ 
+ @AfterClass
+ public static void tearDownClass() {
+ }
+ 
+ @Before
+ public void setUp() {
+ c=new create();
+ 
+ }
+ 
+ @After
+ public void tearDown() {
+ }
 
-class bank
-{
-    
-             Scanner s=new Scanner(System.in);
-            
-            int ch,i;
-             double bal[];
-            double accno[];
-            String address[];
-            String name[];
-            double richest;
-            int n=1,j=2;
-             
-            void create()
-            {
-                
-                 name=new String[10000];
-                 bal=new double[10000];
-                 accno=new double[10000];
-                 address=new String[10000];
-                 
-                 
-                for(i=n;i<j;i++)
-                {
-                   System.out.println("Enter your name: ");
-                    name[i]=s.next() +" " + "" +s.next(); 
-                   
-                    System.out.println("Enter your address: ");
-                    address[i] = s.next();
-                    
-                    
-                    System.out.println("Your account number is: "+i);
-                    System.out.println("Your account has been created" );  
-                    n=j;
-                    
-                } 
-                j++;
-               
-            }
-            
-            
-             
-            void exac()
-            {
-                for(;;)
-                {
-            
-                    
-                    
-                    System.out.println("1.Deposit 2.Withdraw 3.Transfer 4.Balance 5.exit");
-                    System.out.println("Enter your choice");
-                    ch=s.nextInt();
-                    
-                    
-                    switch(ch)
-        {
-            case 1:System.out.println("Enter account number:");
-                    int k = s.nextInt();
-                    i=k;
-                    Scanner s=new Scanner(System.in);
-                    System.out.println("Enter the amount to be deposited");
-                    double damt;
-                    damt = s.nextDouble();
-                     bal[i] += damt;
-                     break;
-                     
-                     
-            case 2: Scanner z=new Scanner(System.in);
-                    System.out.println("Enter account number:");
-                    k = z.nextInt();
-                   i=k;
-                    
-                    Scanner r=new Scanner(System.in);
-                    System.out.println("Enter the amount to be withdrawn");
-                    double wamt;
-                    wamt = r.nextDouble();
-                
-                    if(bal[i]<wamt)
-                
-                    System.out.println("Balance not sufficient");
-                
-                    else
-                    
-                    bal[i] -= wamt;
-                    break;
-                    
-            case 3: Scanner t=new Scanner(System.in);
-                    System.out.println("Enter your account number:");
-                    k = t.nextInt();
-                    i=k;
-                    System.out.println("Enter the amount to be tranferred:");
-                    double transfer=t.nextInt();
-                    
-                    System.out.println("Enter the account number in which the money has to be tranferred:");
-                    int m=t.nextInt();
-                    
-                    bal[m]=bal[m]+(transfer);
-                    bal[k]=bal[k]-(transfer);
-                    
-                    
-                    break;
-                
-                    
-                    
-           
-            case 4:Scanner w=new Scanner(System.in);
-                   System.out.println("Enter account number:");
-                    k = w.nextInt();
-                   i=k;
-              
-                  System.out.println("Current balance is: " +bal[i]);
-                  
-            case 5:return;
-              
-                    }
-                }
-                   
-        
+ @Test
+ public void create() {
+ assertEquals(0, c.addAcc(10,10));
+ assertEquals(20, c.addAcc(20,200));
+ assertEquals(30, c.addAcc(30,345));
+ }
+ 
+ @Test
+ public void deposit() {
+ assertEquals(6000, c.deposit(1000,1020,1020,5000));
+ assertEquals(15000, c.deposit(5000,1021,1021,10000));
+ assertEquals(13800, c.deposit(8800,1022,1022,5000));
+ }
+ 
+ @Test
+ public void withdraw() {
+ assertEquals(500, c.withdraw(500,2099,2099,1000));
+ assertEquals(900, c.withdraw(100,2066,2066,1000));
+ assertEquals(122, c.withdraw(878,2055,2055,1000));
+ }
+ 
+ @Test
+ public void totbal() {
+ assertEquals(5000, c.totbal(5000));
+ }
+ 
+ 
+ @Test
+ public void transfer() {
+ assertEquals(9000, c.transfer(1055,10000,1000,1056));
+ assertEquals(8990, c.transfer(1057,10078,1088,5687));
+ assertEquals(48400, c.transfer(5523,56000,7600,5654));
+ }
+ 
+ @Test
+ public void richest() {
+ assertEquals(50000, c.richest(50000,5500,1));
+ } 
 }
-            
-             void totalbal()
-         {
-             
-            
-            double totalbal=0;
-                    for(i=0;i<3;i++)
-                    {
-                    totalbal=totalbal+bal[i];
-                    }
-                    
-                    System.out.println("Total money in the bank is:"+totalbal);
-         }
-            
-             void richest()
-             {
-                 for(int i=2;i<n;i++)
-                 {
-                     richest=bal[1];
-                     if(bal[i]>richest)
-                     richest=bal[i];
-                 }
-                 System.out.println("The balance of richest person in the bank is:"+richest);   
-                 System.out.println("The name of richest person in the bank is:"+name[i]);   
-             }
-           
-            
-           
-}
-
-public class Banking
-{
-
-    
-    public static void main(String[] args) 
-    {
-        int c;
-        double bal[];
-        double accno[];
-        String address[];
-        String name[];
-        
-        name=new String[10000];
-        bal=new double[10000];
-        accno=new double[10000];
-        address=new String[10000];
-        
-        bank z=new bank();
-        Scanner s=new Scanner(System.in);
-        
-        int i;
-        for(;;)
-         {
-        
-         
-         System.out.println("1.Create new account 2.Open existing account 3.Total money in the bank 4.Richest person in the bank 5.exit");
-         System.out.println("Enter your choice: ");
-         c=s.nextInt();
-        
-        switch(c)
-        {
-            case 1: z. create();  
-                     break;
-                     
-            case 2:z.exac(); 
-                    break;
-                    
-            case 3:z.totalbal();                
-                    break;
-                    
-            case 4: z.richest();
-                        
-                     break; 
-            case 5:break;
-        
-    }
-         }
-    }
-}
-    
-
